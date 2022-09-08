960d0d7a13554bc9565d5bdfe61772bd006d7bc4
Faith Ronana <78912860+FaithB01@users.noreply.github.com>
2022-09-08 21:46:51 +0300

    permisssions

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,7 +1,4 @@
--- user priviledges
-
-CREATE USER 'user_0d_1'@'localhost';
-CREATE USER 'user_0d_2'@'localhost';
-
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+-- script that lists all privileges MySQL Users
+-- user_0d_1 and user_0d_2 on your server
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
