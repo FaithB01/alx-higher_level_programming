b33eb6e51dde49597129ac281b7e9dec349ca59f
root <fnbiketi@gmail.com>
2022-10-03 09:24:42 +0000

    multilanguage

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const firstLine = 'C is fun'
-const secondLine = 'Python is cool'
-const thirdLine = 'JavaScript is amazing'
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
 
-console.log(firstLine +'\n' + secondLine + '\n'+thirdLine)
+console.log(firstLine + '\n' + secondLine + '\n' + thirdLine);
