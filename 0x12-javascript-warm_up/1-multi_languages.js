78d09b272a86b62b1f284ba8081ad9112a3185c0
root <fnbiketi@gmail.com>
2022-10-03 09:11:36 +0000

    js warmup

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const firstLine = 'C is fun'
+const secondLine = 'Python is cool'
+const thirdLine = 'JavaScript is amazing'
+
+console.log(firstLine +'\n' + secondLine + '\n'+thirdLine)
