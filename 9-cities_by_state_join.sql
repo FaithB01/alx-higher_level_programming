4f1d9cb8c3ac270e33480d1bb594b500cf6cf227
Faith Ronana <78912860+FaithB01@users.noreply.github.com>
2022-10-05 22:05:49 +0300

    del

--- 9-cities_by_state_join.sql
+++
@@ -1,8 +0,0 @@
--- Script that lists all cities contained in the database hbtn_0d_usa
-SELECT	cities.id,
-	cities.name,
-	states.name 
-FROM	cities
-JOIN	states
-ON	cities.state_id = states.id
-ORDER BY cities.id ASC;
