10d9145f4df12095ffe9337600a01335fb4794c4
Faith Ronana <78912860+FaithB01@users.noreply.github.com>
2022-10-05 22:03:43 +0300

    objects

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,25 +1,8 @@
 #!/usr/bin/node
-
 const myObject = {
-
   type: 'object',
-
   value: 12
-
 };
-
 console.log(myObject);
-
-/*
-
-YOUR CODE HERE
-
-*/
-let val={
-type:'object',
-value:89
-};
-
-myObject.push(val:89)
-
+myObject.value = 89;
 console.log(myObject);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-
-const myNum='My number: ' 
-console.log(typeof process.argv[2]!= typeof Number(process.argv[2])?myNum +'Not a number' :myNum + process.argv[2])
+const arg = process.argv[2];
+if (isNaN(arg)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + arg);
+}
